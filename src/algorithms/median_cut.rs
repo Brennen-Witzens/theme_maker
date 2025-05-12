944b36f068a28d49ac3953a596f2a7ad9ee71938
Brennen <bwitzen@gmail.com>
2025-05-12 15:16:20 -0700

    Got a median cut 'variant' working

--- src/algorithms/median_cut.rs
+++ src/algorithms/median_cut.rs
@@ -2,6 +2,81 @@ use std::collections::HashMap;
 
 use crate::{utils::find_ranges_for_cube, RGBColor, RGB};
 
+pub fn median_cut(color_cube: &Vec<RGBColor>, iter: i32) -> HashMap<String, Vec<RGBColor>> {
+    let mut cube: Vec<_> = color_cube.iter().cloned().collect();
+    // Sort cube by color values
+    let median: u8;
+    let median_index = cube.len() / 2;
+    let mut above_pixels = Vec::<RGBColor>::new();
+    let mut below_pixels = Vec::<RGBColor>::new();
+    let mut split_cubes = HashMap::<String, Vec<RGBColor>>::new();
+
+    let color_to_cut = find_ranges_for_cube(&cube);
+    match color_to_cut {
+        RGB::Red => {
+            cube.sort_by(|x, y| x.r.cmp(&y.r));
+            if cube.len() % 2 == 0 {
+                median =
+                    ((cube[median_index - 1].r as u16 + cube[median_index].r as u16) / 2) as u8;
+            } else {
+                median = cube[median_index].r;
+            }
+
+            // Now we split the values into above and below buckets
+            for i in 0..cube.len() {
+                if cube[i].r >= median {
+                    above_pixels.push(cube[i]);
+                } else {
+                    below_pixels.push(cube[i]);
+                }
+            }
+        }
+        RGB::Green => {
+            cube.sort_by(|x, y| x.g.cmp(&y.g));
+            if cube.len() % 2 == 0 {
+                median =
+                    ((cube[median_index - 1].g as u16 + cube[median_index].g as u16) / 2) as u8;
+            } else {
+                median = cube[median_index].g;
+            }
+
+            // Now we split the values into above and below buckets
+            for i in 0..cube.len() {
+                if cube[i].g >= median {
+                    above_pixels.push(cube[i]);
+                } else {
+                    below_pixels.push(cube[i]);
+                }
+            }
+        }
+        RGB::Blue => {
+            cube.sort_by(|x, y| x.b.cmp(&y.b));
+            if cube.len() % 2 == 0 {
+                median =
+                    ((cube[median_index - 1].b as u16 + cube[median_index].b as u16) / 2) as u8;
+            } else {
+                median = cube[median_index].b;
+            }
+
+            // Now we split the values into above and below buckets
+            for i in 0..cube.len() {
+                if cube[i].b >= median {
+                    above_pixels.push(cube[i]);
+                } else {
+                    below_pixels.push(cube[i]);
+                }
+            }
+        }
+        RGB::Undefined => {}
+    };
+
+    let above = format!("cubea{}", iter);
+    let below = format!("cubeb{}", iter);
+    split_cubes.insert(above, above_pixels);
+    split_cubes.insert(below, below_pixels);
+    return split_cubes;
+}
+
 // After having found the ranges, we want to sort and cut the color buckets along that color
 // channel.
 // Ex: RGB(12, 6, 19) and RGB(5, 23, 56) sorting along the blue would be 19 < 56
@@ -88,48 +163,52 @@ pub fn median_cut_recursive(color_cube: &Vec<RGBColor>, median_cut_round: i32) {
     median_cut_recursive(&below_pixels, median_cut_round - 1);
 }
 
-// Default here is 'mean' color values. IE Averaging the colors and then returning them.
-fn quantize_values(colors: &Vec<RGBColor>) {
-    //// average them
-    //let mut r_avg;
-    //let mut g_avg;
-    //let mut b_avg;
+// Default here is mean (average) color values
+// -- Currently set to mode (most common) value
+// TODO:
+// - Add a way to change the choice that is returned.
+// - move to utils
+pub fn quantize_values(colors: &Vec<RGBColor>) {
+    // average them
+    let mut r_avg;
+    let mut g_avg;
+    let mut b_avg;
 
-    //let mut r_sum: u32 = 0;
-    //let mut g_sum: u32 = 0;
-    //let mut b_sum: u32 = 0;
-    //for color in colors.iter() {
-    //    r_sum += color.r as u32;
-    //    g_sum += color.g as u32;
-    //    b_sum += color.b as u32;
-    //}
-
-    //r_avg = r_sum / colors.len() as u32;
-    //g_avg = g_sum / colors.len() as u32;
-    //b_avg = b_sum / colors.len() as u32;
-
-    //print!("Color is: {r_avg} - {g_avg} - {b_avg} ->");
-    //println!("\u{001b}[48;2;{};{};{}m    \u{001b}[m", r_avg, g_avg, b_avg);
-
-    // NOTE: Try doing mode
-    let mut most_common = HashMap::<RGBColor, i32>::new();
-
-    for &color in colors {
-        *most_common.entry(color).or_insert(0) += 1;
+    let mut r_sum: u32 = 0;
+    let mut g_sum: u32 = 0;
+    let mut b_sum: u32 = 0;
+    for color in colors.iter() {
+        r_sum += color.r as u32;
+        g_sum += color.g as u32;
+        b_sum += color.b as u32;
     }
 
-    let most_common_color = most_common
-        .into_iter()
-        .max_by_key(|&(_, count)| count)
-        .map(|(val, _)| val)
-        .unwrap();
+    r_avg = r_sum / colors.len() as u32;
+    g_avg = g_sum / colors.len() as u32;
+    b_avg = b_sum / colors.len() as u32;
 
-    print!(
-        "Color is: {} - {} - {} ->",
-        most_common_color.r, most_common_color.g, most_common_color.b
-    );
-    println!(
-        "\u{001b}[48;2;{};{};{}m    \u{001b}[m",
-        most_common_color.r, most_common_color.g, most_common_color.b
-    );
+    print!("Avg Color is: {r_avg} - {g_avg} - {b_avg} ->");
+    println!("\u{001b}[48;2;{};{};{}m    \u{001b}[m", r_avg, g_avg, b_avg);
+
+    //// NOTE: Try doing mode
+    //let mut most_common = HashMap::<RGBColor, i32>::new();
+
+    //for &color in colors {
+    //    *most_common.entry(color).or_insert(0) += 1;
+    //}
+
+    //let most_common_color = most_common
+    //    .into_iter()
+    //    .max_by_key(|&(_, count)| count)
+    //    .map(|(val, _)| val)
+    //    .unwrap();
+
+    //print!(
+    //    "Common Color is: {} - {} - {} ->",
+    //    most_common_color.r, most_common_color.g, most_common_color.b
+    //);
+    //println!(
+    //    "\u{001b}[48;2;{};{};{}m    \u{001b}[m",
+    //    most_common_color.r, most_common_color.g, most_common_color.b
+    //);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod algorithms;
 mod utils;
 
-use algorithms::median_cut::median_cut_recursive;
+use algorithms::median_cut::{self, median_cut, median_cut_recursive, quantize_values};
 use image::GenericImageView;
-use std::hash::Hash;
+use std::{cmp::Ordering, collections::HashMap, hash::Hash, ops::Deref};
+
+use rand::prelude::*;
 
 // Easier struct to work with than to work with the Image crates Rgba struct value of a u8 array
 #[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Copy, Clone)]
@@ -18,12 +20,29 @@ impl RGBColor {
         Self { r, g, b }
     }
 }
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 enum RGB {
     Undefined,
     Red,
     Green,
     Blue,
+}
+
+#[derive(Debug)]
+struct Cube {
+    name: String,
+    color: RGB,
+    range: u8,
+}
+
+impl Cube {
+    fn new(name: &str, color: RGB, range: u8) -> Self {
+        Self {
+            name: name.to_string(),
+            color,
+            range,
+        }
+    }
 }
 
 fn main() -> Result<(), std::io::Error> {
@@ -39,7 +58,140 @@ fn main() -> Result<(), std::io::Error> {
         colors.push(RGBColor::new(color.0[0], color.0[1], color.0[2]));
     }
 
-    median_cut_recursive(&colors, 2);
+    let mut cut_images = HashMap::<String, Vec<RGBColor>>::new();
+    let mut cubes = Vec::<Cube>::new();
+    for i in 0..15 {
+        if i == 0 {
+            cut_images.extend(median_cut(&colors, i).into_iter());
+        } else {
+            // need to go over the cubes to find largest range
+            for (key, val) in cut_images.iter() {
+                // Determine largest range of color in the values (cubes)
+                let cube = determine_cube_cut_criteria(&key, &val);
+                cubes.push(cube);
+            }
+
+            // Iterate over the cubes to determine which cube to use, IE which has the
+            // largest range for a color
+            let val = cubes.iter().max_by_key(|x| x.range).unwrap();
+
+            let cube_to_cut = cut_images.get(&val.name).unwrap();
+            let cub = median_cut(cube_to_cut, i);
+
+            let _ = cut_images.remove_entry(&val.name);
+
+            cut_images.extend(cub.into_iter());
+
+            // Clear cubes at the end of the X
+            cubes.clear();
+        }
+    }
+
+    for colors in cut_images.values() {
+        quantize_values(colors);
+    }
+
+    //median_cut_recursive(&colors, 2);
 
     Ok(())
+}
+
+fn determine_cube_cut_criteria(cube_name: &str, cube: &Vec<RGBColor>) -> Cube {
+    // Iterate over the color cube to find the min and max values for each channel
+
+    // NOTE: Testing for now, will get better handling of unwraps
+    let r_max = cube.iter().max_by(|x, y| x.r.cmp(&y.r)).unwrap();
+    let r_min = cube.iter().min_by(|x, y| x.r.cmp(&y.r)).unwrap();
+    let r_range = r_max.r - r_min.r;
+
+    let g_max = cube.iter().max_by(|x, y| x.g.cmp(&y.g)).unwrap();
+    let g_min = cube.iter().min_by(|x, y| x.g.cmp(&y.g)).unwrap();
+    let g_range = g_max.g - g_min.g;
+
+    let b_max = cube.iter().max_by(|x, y| x.b.cmp(&y.b)).unwrap();
+    let b_min = cube.iter().min_by(|x, y| x.b.cmp(&y.b)).unwrap();
+    let b_range = b_max.b - b_min.b;
+
+    let color_choice;
+    let range;
+    // Determine which range is the largest
+    // NOTE: is there a better way to do this?
+    if r_range > g_range && r_range > b_range {
+        // Red is the largest range
+        color_choice = RGB::Red;
+        range = r_range;
+    } else if g_range > r_range && g_range > b_range {
+        // Green is the largest range
+        color_choice = RGB::Green;
+        range = g_range;
+    } else if b_range > r_range && b_range > g_range {
+        // blue is the largest
+        color_choice = RGB::Blue;
+        range = b_range;
+    } else {
+        let mut rng = rand::rng();
+        // This should be when all of them are equal -> 255 or otherwise
+        if r_range.cmp(&g_range) == Ordering::Equal {
+            // This is where either Green or Red are equal
+            let choice = rng.random_range(0..2);
+            color_choice = match choice {
+                0 => RGB::Red,
+                1 => RGB::Green,
+                _ => RGB::Undefined,
+            };
+            range = match color_choice {
+                RGB::Red => r_range,
+                RGB::Green => g_range,
+                RGB::Blue => b_range,
+                RGB::Undefined => 5, // error cause of sign
+            };
+        } else if r_range.cmp(&b_range) == Ordering::Equal {
+            // This is where either Blue or Red are Equal
+            // Pick one between the two at random
+            let choice = rng.random_range(0..2);
+            color_choice = match choice {
+                0 => RGB::Red,
+                1 => RGB::Blue,
+                _ => RGB::Undefined,
+            };
+            range = match color_choice {
+                RGB::Red => r_range,
+                RGB::Green => g_range,
+                RGB::Blue => b_range,
+                RGB::Undefined => 5, // error cause of sign
+            };
+        } else if g_range.cmp(&b_range) == Ordering::Equal {
+            // This is where Green or Blue are equal
+            let choice = rng.random_range(0..3);
+            color_choice = match choice {
+                0 => RGB::Green,
+                1 => RGB::Blue,
+                _ => RGB::Undefined,
+            };
+            range = match color_choice {
+                RGB::Red => r_range,
+                RGB::Green => g_range,
+                RGB::Blue => b_range,
+                RGB::Undefined => 5, // error cause of sign
+            };
+        } else {
+            // All are equally large pick one are random
+            let choice = rng.random_range(0..3);
+            color_choice = match choice {
+                0 => RGB::Red,
+                1 => RGB::Green,
+                2 => RGB::Blue,
+                _ => RGB::Undefined,
+            };
+            range = match color_choice {
+                RGB::Red => r_range,
+                RGB::Green => g_range,
+                RGB::Blue => b_range,
+                RGB::Undefined => 5, // error cause of sign
+            };
+        }
+    }
+
+    let found_cube = Cube::new(cube_name, color_choice, range);
+    return found_cube;
 }
